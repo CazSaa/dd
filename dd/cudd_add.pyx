9e8bc2c7da5d38e86c52a52b1c4c4297b5dee241
Jules Zisser <jules.zisser@gmail.com>
2025-08-21 13:47:28 +0300

    fix pick_iter

--- dd/cudd_add.pyx
+++ dd/cudd_add.pyx
@@ -1541,6 +1541,7 @@ cdef class ADD:
                         'is not in an ADD '
                         'in this manager')
                 x[index] = g.node
+
             else:
                 x[index] = Cudd_addIthVar(
                     self.manager, index)
@@ -1716,8 +1717,7 @@ cdef class ADD:
               algebraic decision diagram maps
               the assignment `var_values` to.
         """
-        # TODO: with_values
-        return next(self.pick_iter(u, care_vars), None)
+        return next(self.pick_iter(u, care_vars, with_values), None)
 
     def pick_iter(
             self,
@@ -1777,7 +1777,10 @@ cdef class ADD:
                         set(var_values).difference(support))
                 for m in _bdd._enumerate_minterms(
                         var_values, care_vars):
-                    yield m
+                    if with_values:
+                        yield (m, value)
+                    else:
+                        yield m
                 r = Cudd_NextCube(gen, &cube, &value)
         finally:
             Cudd_GenFree(gen)
